4ea592b7adaf707ac4394d9c9bb339d2a6f1c4f3
Stephen Dios <stephendiosdeveloper@gmail.com>
2023-02-03 16:43:19 -0500

    Refactored js functions

--- scripts/script.js
+++ scripts/script.js
@@ -11,88 +11,100 @@ function Book(title, author, currentPages, totalPages, read) {
 
 // TODO extend this to use the DOM input (pushing the add book btn will construct it)
 function addBookToLibrary(book) {    
-    // create divs
-    let bookCard = document.createElement('div');
-    bookCard.classList.add("book-card");
-
-    let bookTopRow = document.createElement('div')
-    bookTopRow.classList.add("book-top-row")
-
-    let bookTitleAuthor = document.createElement('div')
-    bookTitleAuthor.classList.add("book-title-author")
-
-    let bookPages = document.createElement('div')
-    bookPages.classList.add("book-pages")
-
-    ////////////////// book top row //////////////////
-    // delete button with icon
-    let deleteBtn = document.createElement('button');
-    deleteBtn.classList.add("book-delete");
-    let deleteIcon = document.createElement('span');
-    deleteIcon.classList.add("book-delete-icon");
-
-    deleteIcon.innerHTML = "&times";    //need innerHTML here because &times is an html symbol
-    deleteBtn.appendChild(deleteIcon);
-
-    //slider switch
-    let switchLabel = document.createElement('label');
-    switchLabel.classList.add("switch");
-
-    let checkBoxSwitch = document.createElement('input');
-    checkBoxSwitch.setAttribute("type", "checkbox");
-    let sliderSwitch = document.createElement('span');
-    sliderSwitch.classList.add("slider");
-
-    checkBoxSwitch.checked = book.read;
-
-    switchLabel.appendChild(checkBoxSwitch);
-    switchLabel.appendChild(sliderSwitch);
-
-    bookTopRow.appendChild(deleteBtn);
-    bookTopRow.appendChild(switchLabel);
-
-    ////////////////// book title author //////////////////
-    let bookTitle = document.createElement('h3');
-    // bookTitle.innerText = "Book Title";
-    bookTitle.innerText = book.title;
-
-    let bookAuthor = document.createElement('h3');
-    // bookAuthor.innerText = "Book Author";
-    bookAuthor.innerText = book.author;
-
-    bookTitleAuthor.appendChild(bookTitle);
-    bookTitleAuthor.appendChild(bookAuthor);
-
-    ////////////////// book pages //////////////////
-    let firstp = document.createElement('p');
-    firstp.innerText = "Pages: ";
-
-    let currentPage = document.createElement('input');
-    currentPage.setAttribute("type", "number");
-    currentPage.setAttribute("min", "1");
-    currentPage.setAttribute("name", "current-pages");
-    currentPage.setAttribute("id", "current-pages");
-    currentPage.value = book.currentPages;
-
-    let secondp = document.createElement('p');
-    secondp.innerText = "/ " + book.totalPages;
-
-    bookPages.appendChild(firstp);
-    bookPages.appendChild(currentPage);
-    bookPages.appendChild(secondp);
-
-    //combine everything
-    bookCard.appendChild(bookTopRow);
-    bookCard.appendChild(bookTitleAuthor);
-    bookCard.appendChild(bookPages);
-
-    bookshelf.appendChild(bookCard);
+    if(book.currentPages >= book.totalPages) {
+        book.currentPages = book.totalPages;
+        book.read = true;
+    }
+    
     myLibrary.push(book);
+    displayLibrary();
+}
+
+function resetBookshelf() {
+    bookshelf.innerHTML = "";
 }
 
 function displayLibrary() {
-    //for each book in myLibrary
-        //fill out an html template and add to page
+    resetBookshelf();
+
+    myLibrary.forEach(book => {
+        // create divs
+        let bookCard = document.createElement('div');
+        bookCard.classList.add("book-card");
+
+        let bookTopRow = document.createElement('div')
+        bookTopRow.classList.add("book-top-row")
+
+        let bookTitleAuthor = document.createElement('div')
+        bookTitleAuthor.classList.add("book-title-author")
+
+        let bookPages = document.createElement('div')
+        bookPages.classList.add("book-pages")
+
+        ////////////////// book top row //////////////////
+        // delete button with icon
+        let deleteBtn = document.createElement('button');
+        deleteBtn.classList.add("book-delete");
+        let deleteIcon = document.createElement('span');
+        deleteIcon.classList.add("book-delete-icon");
+
+        deleteIcon.innerHTML = "&times";    //need innerHTML here because &times is an html symbol
+        deleteBtn.appendChild(deleteIcon);
+
+        //slider switch
+        let switchLabel = document.createElement('label');
+        switchLabel.classList.add("switch");
+
+        let checkBoxSwitch = document.createElement('input');
+        checkBoxSwitch.setAttribute("type", "checkbox");
+        let sliderSwitch = document.createElement('span');
+        sliderSwitch.classList.add("slider");
+
+        checkBoxSwitch.checked = book.read;
+
+        switchLabel.appendChild(checkBoxSwitch);
+        switchLabel.appendChild(sliderSwitch);
+
+        bookTopRow.appendChild(deleteBtn);
+        bookTopRow.appendChild(switchLabel);
+
+        ////////////////// book title author //////////////////
+        let bookTitle = document.createElement('h3');
+        // bookTitle.innerText = "Book Title";
+        bookTitle.innerText = book.title;
+
+        let bookAuthor = document.createElement('h3');
+        // bookAuthor.innerText = "Book Author";
+        bookAuthor.innerText = book.author;
+
+        bookTitleAuthor.appendChild(bookTitle);
+        bookTitleAuthor.appendChild(bookAuthor);
+
+        ////////////////// book pages //////////////////
+        let firstp = document.createElement('p');
+        firstp.innerText = "Pages: ";
+
+        let currentPage = document.createElement('input');
+        currentPage.setAttribute("type", "number");
+        currentPage.setAttribute("min", "1");
+        currentPage.setAttribute("name", "current-pages");
+        currentPage.setAttribute("id", "current-pages");
+        currentPage.value = book.currentPages;
+
+        let secondp = document.createElement('p');
+        secondp.innerText = "/ " + book.totalPages;
+
+        bookPages.appendChild(firstp);
+        bookPages.appendChild(currentPage);
+        bookPages.appendChild(secondp);
+
+        //combine everything
+        bookCard.appendChild(bookTopRow);
+        bookCard.appendChild(bookTitleAuthor);
+        bookCard.appendChild(bookPages);
+
+        bookshelf.appendChild(bookCard);
+    });
 }
 
 
